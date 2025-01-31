52d2f5aed76dd306c196cfd933dee22b496443ac
Raymond2Moran <124376400+Raymond2Moran@users.noreply.github.com>
2025-01-30 21:57:37 -0500

    Added `hiper_glm.R` and `model_output.R`

---
+++ R/hiper_glm.R
@@ -0,0 +1,6 @@
+hiper_glm <- function(design, outcome) {
+  warning("This function is yet to be implemented.")
+  hglm_out <- list()
+  class(hglm_out) <- "hiper_glm" # Make sure it is S3
+  return(hglm_out)
+}
---
+++ R/model_output.R
@@ -0,0 +1,20 @@
+coef.hiper_glm <- function(hglm_out) {
+  warning("This function is yet to be implemented.")
+}
+
+vcov.hiper_glm <- function(hglm_out) {
+  warning("This function is yet to be implemented.")
+}
+
+print.hiper_glm <- function(hglm_out) {
+  cat("Output of hiper_glm.\n")
+}
+
+# summary <- function() {
+
+# }
+
+# pred <- function() {
+
+# }
+
